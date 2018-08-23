bdf3ddacbcaf46d4aedeff4a5af5602cd6e5cd55
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2018-08-22 21:55:40 -0300

    Estrutura para gravar Resposta está implementada.

---
+++ src/lib/Resposta.php
@@ -0,0 +1,107 @@
+<?php
+class Resposta
+{
+    public $Id;
+    public $Texto;
+    public $Peso;
+    public $Correta;
+    public $NivelProximidadeCorreta;
+
+
+    public $Pergunta;
+    
+
+    function Incluir(&$erros)
+    {
+        $bd     = new BancoDados();
+
+        $id     = $bd->obterNovoUUID();
+
+        if ($id != '')
+        {
+                   
+            $sql    = 'INSERT INTO opcoesrespostas ' .
+                    '(opresposta_id, pergunta_id, opresposta_texto, ' .
+                    'opresposta_peso, opresposta_correta, opresposta_nivelProximidadeCorreta) ' .
+                    'VALUES ' .
+                    "('$id', '$this->Pergunta','$this->Texto', $this->Peso, $this->Correta, $this->NivelProximidadeCorreta)";
+            
+            //echo($sql);
+            $this->Id   = $id;
+            $resultado  = $bd->executar($sql,$erros);
+            return $resultado;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    function Alterar(&$erros){
+        
+        $bd     = new BancoDados();
+
+        if ($this->Id != '')
+        {
+            
+            $sql =  "UPDATE opcoesrespostas" .
+                    "SET opresposta_texto = '$this->Texto'" .
+                    ", opresposta_peso = $this->Peso" .
+                    ", opresposta_correta = $this->Correta" .
+                    ", opresposta_nivelProximidadeCorreta = . $this->NivelProximidadeCorreta" .
+                    " WHERE opresposta_id = '$this->Id'";
+            
+            $resultado  = $bd->executar($sql,$erros);
+            return $resultado;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    static function Listar($pergunta_id)
+    {
+        $lista      = array();
+        $sql        =   'SELECT *  FROM opcoesrespostas ';
+        $where      =   ' WHERE ';              
+       
+        if ($pergunta_id != '') {$sql .= $where . "pergunta_id =  '$pergunta_id'";}      
+
+        $bd         = new BancoDados();
+        $resultado  = $bd->selecionar($sql);
+        if ($resultado != null)
+        {
+            while ($registro = $resultado->fetch_object())
+            {
+                $objeto = new Resposta();
+                $objeto->Id                         = $registro->opresposta_id;
+                $objeto->Pergunta                   = $registro->pergunta_id;
+                $objeto->Texto                      = $registro->opresposta_texto;
+                $objeto->Peso                      = $registro->opresposta_peso;
+                $objeto->Correta                    = $registro->opresposta_correta;
+                $objeto->NivelProximidadeCorreta    = $registro->opresposta_nivelProximidadeCorreta;
+                $lista[] = $objeto;
+            }
+        }
+        return $lista;       
+    }
+
+    static function Deletar($opresposta_id)
+    {
+        
+        $sql        =   "DELETE FROM opcoesrespostas WHERE opresposta_id = '$opresposta_id'";
+        
+        $bd         = new BancoDados();
+        $resultado  = $bd->selecionar($sql);
+        
+        return $resultado;        
+    }
+
+
+    function PrepararWhere(&$sql, &$where, $campo, $operador, $valor)
+    {
+        $sql    .=  "$where $campo $operador '$valor' ";
+        $where  =   'AND ';
+    }
+}
---
+++ src/quiz.aba_resp.php
@@ -0,0 +1,103 @@
+            <h3>Resposta</h3>
+            <div class="w-100">
+                <div class="btn-toolbar" role="toolbar">
+                    <div class="btn-group mr-2" role="group">
+                        <button type="button" class="btn btn-primary">Nova</button>
+                    </div>
+                    <div class="btn-group mr-2" role="group">
+                        <button type="button" class="btn btn-info">
+                            <i class="material-icons">first_page</i>
+                        </button>
+                        <button type="button" class="btn btn-info">
+                            <i class="material-icons">chevron_left</i>
+                        </button>
+                        <button type="button" class="btn btn-info">
+                            <i class="material-icons">chevron_right</i>
+                        </button>
+                        <button type="button" class="btn btn-info">
+                            <i class="material-icons">last_page</i>
+                        </button>                        
+                    </div>
+                    <div class="mr-2 mt-auto mb-auto">
+                        Registro 1/x
+                    </div>
+                    <div class="btn-group ml-auto" role="group">
+                        <button type="button" class="btn btn-danger">Excluir</button>
+                    </div>
+                </div>
+            </div>
+            <br />
+            <form id="formresposta" method="post" enctype="multipart/form-data">
+                <input type="hidden" id="pergunta_id" name="pergunta_id" 
+                <?php if ($pergunta!=NULL) {echo("value='$pergunta->Id'");}?>
+                />
+                <div class="form-group row">
+                    <div class="col-3">
+                        <label for="respcodigo">Id</label>
+                        <input type="texto" class="form-control sm-2" id="respcodigo" name="respcodigo" placeholder="RESPOSTA NOVA" readonly="readonly" 
+                        <?php if ($resposta!=NULL) {echo("value='$resposta->Id'");}?>
+                        />
+                    </div>
+                </div>
+                <div id="divresptexto" class="form-group row">
+                    <div class="col-10">
+                        <label for="enunciadotexto">Texto</label>
+                        <textarea class="form-control" id="enunciadotexto" name="enunciadotexto"><?php if($resposta!=NULL){echo($resposta->Texto);}?></textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-2">
+                        <label for="peso">Peso</label>
+                        <input type="number" class="form-control" id="peso" name="peso" min="1" max="99"  
+                        <?php 
+                        if ($resposta!=NULL){echo("value=$resposta->Peso");}
+                        else {echo("value=0");}
+                        ?>
+                        />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-3">
+                        <label for="respcorreta">Correta</label>
+                        <br />
+                        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-success active">
+                                <input type="radio" name="options" id="correta_true" name="respcorreta" autocomplete="off" value="1" 
+                                <?php  if ($resposta->$Correta) {echo(" checked ");}?>                                
+                                /> Sim
+                            </label>
+                            <label class="btn btn-danger active">
+                                <input type="radio" name="options" id="correta_false" name="respcorreta" autocomplete="off" value="0" 
+                                <?php  if (!$resposta->$Correta) {echo(" checked ");}?>                                
+                                /> Não
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-3">
+                        <label for="nivelproxcorreta">Nível de Proximidade da Resposta Correta</label>
+                        <br />
+                        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-secondary active">
+                                <input type="radio" id="n1" name="nivelproxcorreta" autocomplete="off" value="1" checked /> 1 (Fácil)
+                            </label>
+                            <label class="btn btn-secondary">
+                                <input type="radio" id="n2" name="nivelproxcorreta" autocomplete="off" value="2" /> 2
+                            </label>
+                            <label class="btn btn-secondary">
+                                <input type="radio" id="n3" name="nivelproxcorreta" autocomplete="off" value="3" /> 3
+                            </label>
+                            <label class="btn btn-secondary">
+                                <input type="radio" id="n4" name="nivelproxcorreta" autocomplete="off" value="4" /> 4
+                            </label>
+                            <label class="btn btn-secondary">
+                                <input type="radio" id="n5" name="nivelproxcorreta" autocomplete="off" value="5" /> 5 (Difícil)
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-success">Salvar</button>
+                <button type="button" class="btn btn-warning">Cancelar</button>
+            </form>
+    
