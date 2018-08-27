cdf2d9cad8ab04ed69abbbbb1933a312d6ef7990
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2018-08-27 15:09:37 -0300

    Respostas sendo obtidas por ajax

--- src/js/resposta.js
+++ src/js/resposta.js
@@ -1,4 +1,8 @@
+var jarrResp;
+var respEditada = {};
+
 $(document).ready(function() {
+    /*
     $("#formresposta").on("submit", function(event) {
         if (true)
         {
@@ -24,7 +28,7 @@ $(document).ready(function() {
                         console.log(result_msg.respcodigo);
                         $("#pergcodigo").val(result_msg.respgcodigo);
                     }
-                  /*
+                  
                     if(msg == 'ok'){
                         $('#fupForm')[0].reset();
                         $('.statusMsg').html('<span style="font-size:18px;color:#34A853">Form data submitted successfully.</span>');
@@ -32,7 +36,7 @@ $(document).ready(function() {
                         $('.statusMsg').html('<span style="font-size:18px;color:#EA4335">Some problem occurred, please try again.</span>');
                     }
                   
-                    */
+                    
                    $('#fupForm').css("opacity","");
                    $(".submitBtn").removeAttr("disabled");
                 }
@@ -40,7 +44,14 @@ $(document).ready(function() {
         }
         event.preventDefault();
 
-    });
+    });*/
+    var PerguntaCodigo = $("#pergcodigo").val();
+    if (PerguntaCodigo !== '')
+    {
+       mostrarTabelaResposta();
+       buscarRespostas(PerguntaCodigo);
+       console.log("aqui");
+    }
 });
 
 function ResetFormResposta()
@@ -48,7 +59,6 @@ function ResetFormResposta()
     $( "label.active" ).removeClass( "active" );
 
     $("#formresposta")[0].reset();
-    //$("#pergunta_id").val("");
     $("#respcodigo").val("");
     $("#resptexto").val("");
     $("#peso").val(0);
@@ -63,41 +73,195 @@ function ResetFormResposta()
 
 }
 
-function ExcluirResposta()
+function ExcluirResposta(id)
 {
+    if (id !== "new")
+    {
+        console.log("Excluindo " + id + "do banco");
+    }
     
 }
+function PreparaJsonGravacao(id)
+{ 
+    var pergunta_id = $("#pergcodigo").val();
+    var obj = getRowEditableContent(id);
+    console.log(obj);
+    if (id === "new")
+        id = '';
+    var json = {
+        respcodigo: id,
+        pergunta_id: pergunta_id,
+        resptexto: obj.Texto,
+        respcorreta: obj.Correta,
+        nivelproxcorreta: obj.NivelProximidadeCorreta
+    };
+    return json;
+}
 
-function EnviarResposta()
+function eviarResposta(json)
 {
-   $.post("exec/gravarPergunta.php",json,function(msg){
+    $.post("exec/gravarResposta.php",json,function(msg){
             var result_msg = JSON.parse(msg);
             if(result_msg.resposta === 200)
             {
                 console.log(result_msg.pergcodigo);
                 $("#pergcodigo").val(result_msg.pergcodigo);
                 $("#pergunta_id").val(result_msg.pergcodigo);
-
             }
             resp = result_msg.resposta;
             console.log(resp);
-            MostraModalResposta();
    });
 }
 
-function buscarRespostas(pergunta_id){
-    var json = {id:pergunta_id};
+function buscarRespostas ( pergunta_id, array, i, doneCallback )
+{
+    var json = {pergunta:pergunta_id};
     $.post("exec/buscarRespostas.php",json,function (result){
-        var jarr=JSON.parse(result);
-        var table = gerarTabelaRespostas(jarr);
-        //content.html(table);
-        $("#content").html(table);
-
+        var jarrResp=JSON.parse(result);
+        console.log(jarrResp);
+        //if($("#table_respostas").length === 0)
+        //    mostrarTabelaResposta();
+        //var rows = popularTabelaRespostas(jarrResp);
+        //$("#table_respostas tr:last").after(rows);
+        array[i].arrRespostas = jarrResp;
+        doneCallback();
     });
 }
 
-function gerarTabelaRespostas(jarr){
-    var myTable="";
+function popularTabelaRespostas(jarrResp){
+    var myTable = "";
+    if (jarrResp.length===0)
+    {
+
+        myTable+= "<tr>";
+        myTable+= "<td colspan=\"4\">Esta pergunta ainda não possui opções de resposta</td>";
+        myTable+= "</tr></tbody></table>";
+        return  myTable;
+
+    }
+    for (var i in jarrResp)
+    {
+       myTable += setRowContent(jarrResp[i]);
+    }
+    return myTable;
+}
+
+function criarCelulaOpcoes(id){
+    var html = "";
+    html += "<td >";
+    if(id === "new")
+    {
+        html += "<a href='javascript:void(0);'>";
+    }
+    else {
+        html += "<a href='javascript:void(0);' onClick='editRowRespsposta(\""+id+"\")'>";
+    }
+    html += "<i class='material-icons editar'>create</i></a>";
+    html += "<a href='javascript:void(0);' onClick='ExcluirResposta(\""+id+"\")'>";
+    html += "<i class='material-icons'>delete</i></a>";
+    html += "</td >";
+    return html;
+}
+
+function criarCelulaOpcoesEditar(id){
+    var html = "";
+    html += "<td >";
+    html += "<a href='javascript:void(0);' onClick='salvarResposta(\""+id+"\")'>";
+    html += "<i class='material-icons'>save</i></a>";
+    html += "<a href='javascript:void(0);' onClick='limparAlteracoesResposta(\""+id+"\")'>";
+    html += "<i class='material-icons'>history</i></a>";
+    html += "</td >";
+    return html;
+}
+
+function editRowRespsposta(id)
+{
+    let row = $("#"+id);
+    let obj = getRowContent(id);
+    respEditada = obj;
+    let editableRow = createEditableRowResposta(obj);    
+    
+    row.replaceWith(editableRow);
+}
+
+function getRowContent(id)
+{
+    let row = $("#"+id);
+    let cells = row.children();
+    let obj = new Object();
+    obj.Id = id;
+    obj.Texto = cells[0].innerText ;
+    obj.Correta = cells[1].innerText ;
+    obj.NivelProximidadeCorreta = cells[2].innerText ;
+    return obj;
+    /*
+    for(var i=0;i<3;i++)
+    {
+        var cel = $(cells[i]);
+        console.log($(cells[i]).text());
+        var tmp = cel.text();
+        cel.html("<input type='text' value='" + tmp + "'>");       
+    }*/
+}
+
+function setRowContent(rowObj)
+{
+    let rowHtml = "<tr id=\""+rowObj.Id+"\">";
+    rowHtml+="<td>"+rowObj.Texto+"</td>";
+    rowHtml+="<td>"+rowObj.Correta+"</td>";
+    rowHtml+="<td>"+rowObj.NivelProximidadeCorreta+"</td>";
+    rowHtml+= criarCelulaOpcoes(rowObj.Id);
+    rowHtml+="</tr>";
+    return rowHtml;
+}
+
+function getRowEditableContent(id)
+{
+    let row = $("#"+id);
+    let cells = row.children();
+    let obj = new Object();
+    obj.Id = id;
+    obj.Texto = cells[0].lastChild.value ;
+    obj.Correta = cells[1].lastChild.value ;
+    obj.NivelProximidadeCorreta = cells[2].lastChild.value ;
+    return obj;
+}
+
+function createEditableRowResposta(obj)
+{
+    let newRow = "<tr id=\""+ obj.Id +"\">";
+    newRow += createTd(createInputField(obj.Texto));
+    newRow += createTd(createComboBox(obj.Correta));
+    newRow += createTd(createComboBox(obj.NivelProximidadeCorreta));
+    newRow += criarCelulaOpcoesEditar(obj.Id);
+    newRow +="</tr>";
+    return newRow;
+}
+
+function createNewRow(){
+    let obj = {Id:"new", Texto:"", Correta:"", NivelProximidadeCorreta:""};
+    let row = createEditableRowResposta(obj);
+    $("#novalinha").after(row);    
+}
+
+function createTd(html)
+{
+    return "<td>" + html + "</td>";
+}
+
+function createInputField(value)
+{
+    return "<input type='text' value='" + value + "'>";
+}
+
+function createComboBox(value)
+{
+    return "<input type='text' value='" + value + "'>";
+}
+
+function mostrarTabelaResposta(){
+    var header = "<hr /><h3>Respostas</h3><br />";
+    var myTable= header;    
     myTable = "<table id=\"table_respostas\" class=\"table table-striped\">";
     myTable+= "<thead class=\"thead-dark\">";
     myTable+= "<tr>";
@@ -107,40 +271,29 @@ function gerarTabelaRespostas(jarr){
     myTable+= "<th scope=\"col\">Opções</th>";
     myTable+= "</tr></thead>";
     myTable+= "<tbody>";
-    if (jarr.length===0)
-    {
-
-        myTable+= "<tr>";
-        myTable+= "<td colspan=\"4\">Não foram encontrados clientes com os parâmetros informados</td>";
-        myTable+= "</tr></tbody></table>";
-        return  myTable;
-
-    }
-    for (var i in jarr)
-    {
-        myTable+="<tr id=\""+jarr[i].Id+"\">";
-        myTable+="<td>"+jarr[i].Texto+"</td>";
-        myTable+="<td>"+jarr[i].Correta+"</td>";
-        myTable+="<td>"+jarr[i].NivelProximidadeCorreta+"</td>";
-        myTable+=criarCelulaOpcoes(jarr[i].Id);
-        myTable+="</tr>";
-    }
-        myTable+="</tbody></table>";
-        return myTable;
+    myTable+= "<tr id=\"novalinha\">";
+    myTable+= "<td colspan=\"6\">";
+    myTable+= "<button id=\"novaresposta\" class=\"btn btn-link\"";
+    myTable+= " onClick='createNewRow()'";
+    myTable+= " >";
+    myTable+= "<i class=\"material-icons\">add_circle</i> Nova Opção";
+    myTable+= "</button>";
+    myTable+= "</td>";
+    myTable+= "</tr>";
+    myTable+="</tbody></table>";
+    $("#content").html(myTable);
+    //return myTable;
 }
 
-function criarCelulaOpcoes(id){
-    var html = "";
-    html += "<td >";
-    html += "<a href='javascript:void(0);' onclick='editRowRespsposta(\""+id+"\")'>";
-    html += "<i class='material-icons'>create</i></a>";
-    html += "<a href='#'>";
-    html += "<i class='material-icons'>delete</i></a>";
-    html += "</td >";
-    return html;
+function salvarResposta(id)
+{
+    console.log("Salvando " + id);
+    var json = PreparaJsonGravacao(id);
+    eviarResposta(json);
+    
 }
 
-function editRowRespsposta(){
-
-
+function limparAlteracoesResposta(id)
+{
+    console.log("Limpando alteracoes de " + id);
 }
